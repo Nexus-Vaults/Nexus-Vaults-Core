6a24b14bdb5a1035c8b9bb951f66f469655349ab
Playwo <eliaswolf2001@t-online.de>
2023-05-14 15:17:21 +0000

    Add common utils contracts

---
+++ contracts/utils/ERC165Checker.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+abstract contract ERC165Checker {
+  function _supportsERC165Interface(
+    address account,
+    bytes4 interfaceId
+  ) internal view returns (bool) {
+    bytes memory encodedParams = abi.encodeWithSelector(
+      IERC165.supportsInterface.selector,
+      interfaceId
+    );
+    (bool success, bytes memory result) = account.staticcall{gas: 30000}(
+      encodedParams
+    );
+    if (result.length < 32) return false;
+    return success && abi.decode(result, (bool));
+  }
+}
