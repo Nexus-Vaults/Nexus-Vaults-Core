4fe16d3f0cf68d43e27b6732a20b35031e28bd6f
Playwo <eliaswolf2001@t-online.de>
2023-05-27 22:24:00 +0200

    Install contract sizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-contract-sizer';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.18',
+  solidity: {
+    version: '0.8.18',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 50,
+      },
+    },
+  },
 };
 
 export default config;
